203c34a933b8f92675c6194f213aceccf4e7c552
Wiebe <wiebe.willems@live.be>
2020-05-29 22:45:03 +0200

    Adding terminator installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 # Preparing all .sh scripts to be able to get executed
 find ./ -type f -iname "*.sh" -exec chmod +x {} \;
 
+# Install Terminator
+./terminator/install.sh
---
+++ terminator/install.sh
@@ -0,0 +1,5 @@
+# Install terminator from apt
+sudo apt-get install terminator
+
+# Setting terminator as default terminal
+sudo update-alternatives --config x-terminal-emulator
